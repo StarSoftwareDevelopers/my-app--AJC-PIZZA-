c6d83651a2b4c5b563ac640f1b365362cdfd6584
StarSoftwareDevelopers <starsoftwaredev@gmail.com>
2021-03-11 12:20:17 +0800

    edited the input fields

--- src/components/Forms/FormInput/index.js
+++ src/components/Forms/FormInput/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
-// import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => {
     return ( 
         <div className="formRow">
-            {label && (
-                <label {...otherProps}>
-                    {label}
-                </label>
-            )}
-            {/* <TextField
+
+            <TextField
             autoFocus
             margin="dense"
-            id="name"
-            label={"Email Address"}
-            type="email"
-            fullWidth
-          /> */}
-            <input className="formInput" onChange={handleChange} {...otherProps}/>
+            label={label}
+            fullWidth 
+            variant="outlined"
+            onChange={handleChange}
+            {...otherProps}
+          />
+            {/* <input className="formInput" onChange={handleChange} {...otherProps}/> */}
         </div>
      );
 }
