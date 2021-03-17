7890a61c3ff3e64ec2516366bfc50734e7585f14
StarSoftwareDevelopers <bg201801625@wmsu.edu.ph>
2021-03-17 17:33:55 +0800

    appbar

--- src/Admin/AdminComponents/AppBar.js
+++ src/Admin/AdminComponents/AppBar.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
-import {Link} from 'react-router-dom';
-import HomeIcon from '@material-ui/icons/Home';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import GroupIcon from '@material-ui/icons/Group';
-import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
-import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { Link } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import GroupIcon from "@material-ui/icons/Group";
+import PeopleOutlineIcon from "@material-ui/icons/PeopleOutline";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-import { Route, Switch } 
-  from 'react-router-dom'; 
+import { Route, Switch } from "react-router-dom";
 
-  import Home from './../Pages/Home';
-  import Menu from './../Pages/Menu';
-  import Users from './../Pages/User';
+import Home from "./../Pages/Home";
+import Menu from "./../Pages/Menu";
+import Users from "./../Pages/User";
 
-import './../Admin.scss';
-import Notifications from '../Pages/Notifications';
-import Orders from '../Pages/Orders';
-
+import "./../Admin.scss";
+import Notifications from "../Pages/Notifications";
+import Orders from "../Pages/Orders";
+import AdminMgt from "../Pages/Admin";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
     backgroundColor: "white",
     color: "orange",
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -55,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -64,40 +63,40 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
 
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    fontSize: '20px',
+    fontSize: "20px",
   },
   drawerPaper: {
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: '20px',
+    display: "flex",
+    alignItems: "center",
+    fontSize: "20px",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -137,24 +136,26 @@ export default function PersistentDrawerLeft() {
           >
             <MenuIcon fontSize="large" />
           </IconButton>
-          <Typography variant="h4" noWrap
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        className= {classes.title}
+          <Typography
+            variant="h4"
+            noWrap
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            className={classes.title}
           >
-            <Link to="/admin">
-              AJC HOMEMADE PIZZA
-            </Link>
+            <Link to="/admin">AJC HOMEMADE PIZZA</Link>
           </Typography>
           <div>
-            <Typography noWrap variant="h5"
-            style={{
-              padding: '1rem'
-            }}>
+            <Typography
+              noWrap
+              variant="h5"
+              style={{
+                padding: "1rem",
+              }}
+            >
               Welcome, Admin
             </Typography>
           </div>
-
         </Toolbar>
       </AppBar>
       <Drawer
@@ -168,90 +169,84 @@ export default function PersistentDrawerLeft() {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-          <Link to='/admin'>  
-                <ListItem button key='Home'> 
-                  <ListItemIcon><HomeIcon fontSize="large"/> 
-                  </ListItemIcon> 
-                  <ListItemText>
-                    <Typography variant="h5">
-                      Home
-                    </Typography>
-                  </ListItemText>
-                </ListItem>  
-            </Link> 
-          <Link to='/Menu'> 
-                <ListItem button key='Menu'> 
-                  <ListItemIcon><MenuBookIcon fontSize="large"/> 
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Menu
-                    </Typography> 
-                </ListItem> 
+          <Link to="/admin">
+            <ListItem button key="Home">
+              <ListItemIcon>
+                <HomeIcon fontSize="large" />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography variant="h5">Home</Typography>
+              </ListItemText>
+            </ListItem>
           </Link>
-          <Link to='/Orders'> 
-                <ListItem button key='Orders'> 
-                  <ListItemIcon><ShoppingCartIcon fontSize="large"/> 
-                  </ListItemIcon> 
-                    <Typography variant="h5">
-                        Orders
-                    </Typography> 
-                </ListItem> 
-           </Link>  
-           <Link to='/Notifications'> 
-                <ListItem button key='Notifications'> 
-                  <ListItemIcon><NotificationsIcon fontSize="large"/>
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Notifications
-                    </Typography>
-                </ListItem> 
-           </Link>
-           <Divider/>
-           <Link to='/AdminMgt'> 
-                <ListItem button key='Admin'> 
-                  <ListItemIcon><SupervisedUserCircleIcon fontSize="large"/>
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Admin
-                    </Typography> 
-                </ListItem> 
-           </Link>
-           <Link to='/Staff'> 
-                <ListItem button key='Staff'> 
-                  <ListItemIcon><GroupIcon fontSize="large"/>
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Staff
-                    </Typography>
-                </ListItem> 
-           </Link>
-           <Link to='/Users'> 
-                <ListItem button key='Users'> 
-                  <ListItemIcon><PeopleOutlineIcon fontSize="large"/>
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Users
-                    </Typography>
-                </ListItem> 
-           </Link>
-         
-                <ListItem button key='Users'> 
-                  <ListItemIcon><ExitToAppIcon fontSize="large"/>
-                  </ListItemIcon> 
-                  <Typography variant="h5">
-                      Log Out
-                    </Typography>
-                </ListItem> 
-           
-        </List>
+          <Link to="/Menu">
+            <ListItem button key="Menu">
+              <ListItemIcon>
+                <MenuBookIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Menu</Typography>
+            </ListItem>
+          </Link>
+          <Link to="/Orders">
+            <ListItem button key="Orders">
+              <ListItemIcon>
+                <ShoppingCartIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Orders</Typography>
+            </ListItem>
+          </Link>
+          <Link to="/Notifications">
+            <ListItem button key="Notifications">
+              <ListItemIcon>
+                <NotificationsIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Notifications</Typography>
+            </ListItem>
+          </Link>
+          <Divider />
+          <Link to="/AdminMgt">
+            <ListItem button key="Admin">
+              <ListItemIcon>
+                <SupervisedUserCircleIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Admin</Typography>
+            </ListItem>
+          </Link>
+          <Link to="/Staff">
+            <ListItem button key="Staff">
+              <ListItemIcon>
+                <GroupIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Staff</Typography>
+            </ListItem>
+          </Link>
+          <Link to="/Users">
+            <ListItem button key="Users">
+              <ListItemIcon>
+                <PeopleOutlineIcon fontSize="large" />
+              </ListItemIcon>
+              <Typography variant="h5">Users</Typography>
+            </ListItem>
+          </Link>
 
+          <ListItem button key="Users">
+            <ListItemIcon>
+              <ExitToAppIcon fontSize="large" />
+            </ListItemIcon>
+            <Typography variant="h5">Log Out</Typography>
+          </ListItem>
+        </List>
       </Drawer>
-     
+
       <main
         className={clsx(classes.content, {
           [classes.contentShift]: open,
@@ -259,11 +254,12 @@ export default function PersistentDrawerLeft() {
       >
         <div className={classes.drawerHeader} />
         <Switch>
-          <Route exact path='/admin' component={Home}/> 
-          <Route exact path='/Menu' component={Menu}/> 
-          <Route exact path='/Orders' component={Orders}/> 
-          <Route exact path='/Notifications' component={Notifications}/> 
-          <Route exact path='/Users' component={Users}/> 
+          <Route exact path="/admin" component={Home} />
+          <Route exact path="/Menu" component={Menu} />
+          <Route exact path="/Orders" component={Orders} />
+          <Route exact path="/Notifications" component={Notifications} />
+          <Route exact path="/AdminMgt" component={AdminMgt} />
+          <Route exact path="/Users" component={Users} />
         </Switch>
       </main>
     </div>
