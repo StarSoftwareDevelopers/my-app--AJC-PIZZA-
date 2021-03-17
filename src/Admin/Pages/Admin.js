1917f8653f3e4a56be765cf51f3c06914235d351
StarSoftwareDevelopers <bg201801625@wmsu.edu.ph>
2021-03-17 17:34:14 +0800

    trying for admin

---
+++ src/Admin/Pages/Admin.js
@@ -0,0 +1,14 @@
+const AdminMgt = () => {
+  return (
+    <div>
+      Admin
+      <form>
+        <input type="text" label="Name" placeholder="Name" />
+        <input type="email" placeholder="admin" />
+        <button type="submit">Make Admin</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminMgt;
