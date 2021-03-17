57f0a9602fcabc129ade56fd19555d27bd69e943
StarSoftwareDevelopers <bg201801625@wmsu.edu.ph>
2021-03-17 17:46:35 +0800

    firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 import { firebaseConfig } from "./config";
 
 firebase.initializeApp(firebaseConfig);
@@ -14,44 +14,43 @@ export default firebase;
 
 //utility function for the onclick event with the Sign in of Google button
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
-GoogleProvider.setCustomParameters({prompt: 'select_account' });
+GoogleProvider.setCustomParameters({ prompt: "select_account" });
 
 export const FacebookProvider = new firebase.auth.FacebookAuthProvider();
-FacebookProvider.setCustomParameters({prompt: 'select_account'});
+FacebookProvider.setCustomParameters({ prompt: "select_account" });
 
-export const handleUserProfile = async ({userAuth, additionalData}) => {
-    if (!userAuth) return;
-    const {uid} = userAuth;
+export const handleUserProfile = async ({ userAuth, additionalData }) => {
+  if (!userAuth) return;
+  const { uid } = userAuth;
 
-    const userRef = firestore.doc(`users/${uid}`);
-        
-    //create new user
-    const snapshot = await userRef.get();
-    if (!snapshot.exists){
-        const { displayName, email} = userAuth;
-        const timestamp = new Date();
-        //if the user exist  does not exist
-        try{
-            await userRef.set({
-                displayName,
-                email,
-                createdDate: timestamp,
-                ...additionalData
-            });
+  const userRef = firestore.doc(`users/${uid}`);
 
-        }catch(err){
-            console.log(err);
-        }
+  //create new user
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const timestamp = new Date();
+    //if the user exist  does not exist
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdDate: timestamp,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log(err);
     }
-    return userRef;
+  }
+  return userRef;
 };
 
 //if the currentUser is signed in in the application
 export const getCurrentUser = () => {
-    return new Promise((resolve, reject) => {
-        const unsubscribe = auth.onAuthStateChanged(userAuth => {
-            unsubscribe();
-            resolve(userAuth); //this tell us if the user is signed in with the application or not 
-        }, reject);
-    })
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
 };
