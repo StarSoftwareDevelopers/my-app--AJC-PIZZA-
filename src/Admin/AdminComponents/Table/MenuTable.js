527922e88f380cf7d21be04edc7009735ca19982
StarSoftwareDevelopers <bg201801625@wmsu.edu.ph>
2021-03-27 15:58:10 +0800

    modal for add products

--- src/Admin/AdminComponents/Table/MenuTable.js
+++ src/Admin/AdminComponents/Table/MenuTable.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
 
 const columns = [
-  { id: 'name', label: 'Product', minWidth: 170 },
-  { id: 'image', label: 'Image', minWidth: 100 },
-  { id: 'desc', label: 'Description', minWidth: 100 },
-  { id: 'price', label: 'Price', minWidth: 100 },
-  { id: 'actions', label: 'Action', minWidth: 100 },
+  { id: "name", label: "Product", minWidth: 170 },
+  { id: "image", label: "Image", minWidth: 100 },
+  { id: "desc", label: "Description", minWidth: 100 },
+  { id: "price", label: "Price", minWidth: 100 },
+  { id: "actions", label: "Action", minWidth: 100 },
 ];
 
 function createData(name, code, population, size) {
@@ -23,13 +23,13 @@ function createData(name, code, population, size) {
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
- //row data
+  createData("", "IN", 1324171354, 3287263),
+  //row data
 ];
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: "100%",
   },
   container: {
     maxHeight: 440,
@@ -68,25 +68,29 @@ export default function StickyHeadTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[5,10, 25, 100]}
+        rowsPerPageOptions={[5, 10, 25, 100]}
         component="div"
         count={rows.length}
         rowsPerPage={rowsPerPage}
--- src/Admin/Pages/Menu.js
+++ src/Admin/Pages/Menu.js
@@ -1,35 +1,110 @@
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import {Link} from 'react-router-dom';
-import './../Admin.scss';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import MenuTable from './../AdminComponents/Table/MenuTable';
+import React from "react";
+import { InputAdornment } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
+import "./../Admin.scss";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import MenuTable from "./../AdminComponents/Table/MenuTable";
+
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const Menu = () => {
-    return ( 
-        <Container maxWidth = "lg" style={{
-            margin: "1rem"
-        }}>
-    
-              <Typography variant="h5" style={{
-                  textAlign: "left",
-                  marginTop: "-20px",
-                  display: 'inline-block',
-                  marginRight: '1rem'
-                  }}>Menu</Typography>
-                  
-                <Card style={{padding: '1rem'}}>
-                <Link to="#">
-                    <Button variant="outlined" color="primary">
-                        Add New Menu
-                    </Button>
-                </Link>
-                <br></br><br></br>
-                <MenuTable/>
-                </Card>
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Container
+      maxWidth="lg"
+      style={{
+        margin: "1rem",
+      }}
+    >
+      <Typography
+        variant="h5"
+        style={{
+          textAlign: "left",
+          marginTop: "-20px",
+          display: "inline-block",
+          marginRight: "1rem",
+        }}
+      >
+        Menu
+      </Typography>
+
+      <Card style={{ padding: "1rem" }}>
+        <Link to="#">
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Add New Menu
+          </Button>
+        </Link>
+        <br></br>
+        <br></br>
+        <MenuTable />
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add New Menu</DialogTitle>
+          <DialogContent>
+            <form>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="product"
+                label="Product Name"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                margin="dense"
+                id="desc"
+                label="Product Description"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                margin="dense"
+                id="price"
+                label="Price"
+                type="number"
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start"> ₱</InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="start">.00</InputAdornment>
+                  ),
+                }}
+              />
+              <input type="file" name="myImage" accept="image/*" />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Card>
     </Container>
-     );
-}
- 
+  );
+};
+
 export default Menu;
