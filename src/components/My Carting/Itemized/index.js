16d54c2f77255d898d3cab40bf09a6c36cce240b
StarSoftwareDevelopers <bg201801625@wmsu.edu.ph>
2021-03-30 12:03:26 +0800

    fixed buttons in my cart

--- src/components/My Carting/Itemized/index.js
+++ src/components/My Carting/Itemized/index.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import Chicken from './../../../assets/chickenpizza.jpg';
-import Tuna from './../../../assets/tunapizza.jpg';
-import Vegetable from './../../../assets/vegetablepizza.jpg';
-import Hawaiian from './../../../assets/hawaiianpizza.jpg';
-import Button from './../../Forms/Button';
-
-import './../style.scss';
+import React from "react";
+import Chicken from "./../../../assets/chickenpizza.jpg";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import "./../style.scss";
 
 const Itemized = ({}) => {
-    return (
-        <table className="cartItems" cellPadding="10" cellSpacing="0" border="0">
-            <tbody>
-                <tr>
-                    <td>
-                        <img src={Chicken} alt="chicken"/>
-                    </td>
-                    <td>
-                        Chicken Pizza
-                    </td>
-                    <td>
-                        <span>1</span>
-                    </td>
-                    <td>
-                        ₱ 130.00
-                    </td>
-                    <td className="buttn">
-                    <a>X</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table className="cartItems" cellPadding="10" cellSpacing="0" border="0">
+      <tbody>
+        <tr>
+          <td>
+            <img src={Chicken} alt="chicken" />
+          </td>
+          <td>Chicken Pizza</td>
+          <td>
+            <IconButton>
+              <ArrowBackIosIcon size="small" color="secondary" />
+            </IconButton>
+            <span>1</span>
+            <IconButton>
+              <ArrowForwardIosIcon size="small" color="secondary" />
+            </IconButton>
+          </td>
+          <td>₱ 130.00</td>
+          <td>
+            <IconButton>
+              <HighlightOffIcon fontSize="large" color="secondary" />
+            </IconButton>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 export default Itemized;
